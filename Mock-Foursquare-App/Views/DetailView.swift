be2c7526ca500de9d96aff3446ce45300e520032
Eddie O'Neill <ejosephoneill@gmail.com>
2020-02-25 11:56:21 -0500

    added swift files for table view and detail view

---
+++ Mock-Foursquare-App/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Mock-Foursquare-App
+//
+//  Created by Edward O'Neill on 2/25/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
