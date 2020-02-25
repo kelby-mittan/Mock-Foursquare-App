8f9458253aa5d3329434bca8090429a28917c92a
Eddie O'Neill <ejosephoneill@gmail.com>
2020-02-25 12:27:24 -0500

    added 4 swift for table view and detail view

---
+++ Mock-Foursquare-App/View Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Mock-Foursquare-App
+//
+//  Created by Edward O'Neill on 2/25/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mock-Foursquare-App/Views/ItemTableView.swift
@@ -0,0 +1,15 @@
+//
+//  ItemTableView.swift
+//  Mock-Foursquare-App
+//
+//  Created by Edward O'Neill on 2/25/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableView: UIView {
+    
+    
+
+}
