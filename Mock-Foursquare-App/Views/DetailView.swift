d1d47da5c35e0e6457182fabd3b7741d155c7ac9
Eddie O'Neill <ejosephoneill@gmail.com>
2020-02-26 14:07:08 -0500

    setted up the detail view

--- Mock-Foursquare-App/Views/DetailView.swift
+++ Mock-Foursquare-App/Views/DetailView.swift
@@ -10,12 +10,109 @@ import UIKit
 
 class DetailView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public lazy var itemImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "camera.on.rectangle")
+        return iv
+    }()
+    
+    public lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "title"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    public lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.text = "address"
+        label.textColor = .systemGray
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var savePicker: UIPickerView! = {
+        let pv = UIPickerView(frame: CGRect.zero)
+        pv.backgroundColor = .systemBackground
+        return pv
+    }()
+    
+//    public lazy var descriptionTextView: UITextView = {
+//        let tv = UITextView()
+//        tv.text = "description----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+//        return tv
+//    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupImageViewConstraints()
+        setupTitleViewConstraints()
+        setupAddressViewConstraints()
+        setupPickerViewConstraints()
+    }
+    
+    private func setupImageViewConstraints() {
+        addSubview(itemImage)
+        itemImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            itemImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            itemImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            itemImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            itemImage.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+    
+    private func setupTitleViewConstraints() {
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: itemImage.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    private func setupAddressViewConstraints() {
+        addSubview(addressLabel)
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addressLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            addressLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            addressLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            addressLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+//    private func setupDescriptionViewConstraints() {
+//        addSubview(descriptionTextView)
+//        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            descriptionTextView.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 5),
+//            descriptionTextView.leadingAnchor.constraint(equalTo: leadingAnchor),
+//            descriptionTextView.trailingAnchor.constraint(equalTo: trailingAnchor),
+//            descriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor)
+//        ])
+//    }
+    
+    private func setupPickerViewConstraints() {
+        addSubview(savePicker)
+        savePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            savePicker.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 20),
+            savePicker.leadingAnchor.constraint(equalTo: leadingAnchor),
+            savePicker.trailingAnchor.constraint(equalTo: trailingAnchor),
+            savePicker.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 
 }
